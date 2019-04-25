898c04cf1ab58c9903c693096a22d6f843cfe3b2
Adam Harrison <harrisonadamw@gmail.com>
2019-04-25 15:32:19 +0100

    First actual DFT of audio samples

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,23 @@
 module Main where
 
 import Math.FFT (dftRC)
+import Data.Array.IArray (listArray, amap)
 import Data.Array.CArray (CArray)
-import Data.Complex (Complex)
+import Data.Complex (Complex, magnitude)
 
 import Sound.Pulse.Simple
 
 foo :: CArray Int Float -> CArray Int (Complex Float)
 foo = dftRC
 
+bar :: [Float] -> CArray Int Float
+bar fs = listArray (0, (length fs - 1)) fs
+
 main :: IO ()
 main = do
     s <- simpleNew Nothing "example" Record Nothing "this is an example application"
         (SampleSpec (F32 LittleEndian) 44100 1) Nothing Nothing
-    xs <- simpleRead s $ 44100*10 :: IO [Float]
+    xs <- simpleRead s (128) :: IO [Float]
     simpleFree s
+    let res = foo . bar
+    putStrLn $ show $ amap magnitude $ res xs
