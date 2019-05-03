510a492c56ca70b626f123149213b0a41507947f
Adam Harrison <harrisonadamw@gmail.com>
2019-05-03 13:28:18 +0100

    Render frequency bins as bars

--- app/Main.hs
+++ app/Main.hs
@@ -14,7 +14,7 @@ import Graphics.Gloss.Interface.IO.Animate (animateIO)
 import Graphics.Gloss.Data.Color (black,white)
 import Graphics.Gloss.Data.Controller (Controller)
 import Graphics.Gloss.Data.Display (Display(..))
-import Graphics.Gloss.Data.Picture (Picture(..),Point,blank,line)
+import Graphics.Gloss.Data.Picture
 
 source = "alsa_output.usb-AudioQuest_AudioQuest_DragonFly_Black_v1.5_AQDFBL0100116667-00.analog-stereo.monitor"
 
@@ -43,7 +43,8 @@ render samples =
         renderFrame :: Float -> IO Picture
         renderFrame _ = do
             ss <- atomically $ readTVar samples
-            return $ line $ samplesToPath (-256) $ fmap log $ fft ss
+            --return $ line $ samplesToPath (-256) $ fmap log $ fft ss
+            return $ Pictures $ samplesToBars (-256) $ fft ss
 
         fft :: [Float] -> [Float]
         fft ss = elems $ amap magnitude $ dftRC $ listArray (0, (length ss - 1)) ss
@@ -51,6 +52,12 @@ render samples =
         samplesToPath :: Float -> [Float] -> [Point]
         samplesToPath _ [] = []
         samplesToPath i (s:ss) = (i, s * 10) : samplesToPath (i + 1) ss
+
+        samplesToBars :: Float -> [Float] -> [Picture]
+        samplesToBars _ [] = []
+        samplesToBars i (s:ss) = bin : samplesToBars (i + 1) ss
+            where
+                bin = translate (i * 6) (s * 5) $ rectangleSolid 5 (s * 10)
 
         callback :: Controller -> IO ()
         callback _ = return ()
